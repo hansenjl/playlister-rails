f14e5411bf8f8eb51f603e9496ae49da3a4f0762
hansenjl <jenn.leigh.hansen@gmail.com>
2019-04-23 14:36:50 -0700

    update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root: '/playlists'
   resources :artists
   resources :songs
   resources :playlists
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+artists = Artist.create([{name: 'Taylor Swift'}, {name: 'Kanye West'}, {name: "Adele"}, {name:"Pink"}, {name:"Beach Boys"},{name: "Beatles"}, {name:"Ariana Grande"}, {name:"Ed Sheeran"}, {name:"Katy Perry"}])
+
+Song.create([{name: "Out of the Woods", genre: 'pop', artist_id: Artist.first.id},{name: "Blank Space", genre: 'pop', artist_id: Artist.first.id}])
