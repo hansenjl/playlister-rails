91ef7214ffdaf930549474eebd4be3ea24492d5f
hansenjl <jenn.leigh.hansen@gmail.com>
2019-04-23 14:43:49 -0700

    add in controller actions

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,22 @@
 class PlaylistsController < ApplicationController
+  def index
+    @playlists = Playlist.all
+  end
+
+  def show
+    @playlist = Playlist.find_by_id(params[:id])
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new
+    if @playlist.save
+      redirect_to playlist_path(@playlist)
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,22 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find_by_id(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
 end
